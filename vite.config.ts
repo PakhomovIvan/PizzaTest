24d9adb76a0a91a6d6b58473ca147f7f8a68fd80
Ivan Pakhomov <ip1133@ya.ru>
2025-03-26 15:35:40 +0300

    fix: change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['v2732210.hosted-by-vdsina.ru'],
     watch: {
       usePolling: true,
     },
