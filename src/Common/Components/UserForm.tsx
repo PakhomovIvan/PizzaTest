aa5cc7ec757d53f8e60c109c1aa5472e9e0c79c2
Ivan Pakhomov <ip1133@ya.ru>
2025-03-27 16:17:49 +0300

    fix: form style

--- src/Common/Components/UserForm.tsx
+++ src/Common/Components/UserForm.tsx
@@ -174,7 +174,6 @@ const UserForm = ({ onSubmit, isLoadingData, userData }: UserFormProps) => {
         <Link to="/users-list">
           <Button label="Назад" severity="contrast" />
         </Link>
-
         <Button
           label={userData ? 'Сохранить' : 'Создать'}
           severity="contrast"
